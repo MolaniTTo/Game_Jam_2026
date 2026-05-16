f3d0f4b24e16ead1aceedc22d31efe1f706a06aa
nilbalaguer <nilmoixaines@gmail.com>
2026-05-17 00:17:37 +0200

    Les dos mans estan fetes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -42,6 +42,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Sprite puntero1;
     [SerializeField] Sprite puntero2;
 
+    [SerializeField] MeshRenderer meshMaTancada;
+
     private void Start()
     {
         // Bloquear cursor
@@ -61,6 +63,7 @@ public class PlayerController : MonoBehaviour
 
         particulesSargantana = gameObject.GetComponentInChildren<ParticleSystem>();
 
+        meshMaTancada.enabled = false;
 
     }
 
@@ -161,6 +164,7 @@ public class PlayerController : MonoBehaviour
         sargantanaAgafadaInstance.transform.localRotation = Quaternion.identity;
         sargantanaAgafada = true;
 
+        meshMaTancada.enabled = true;
         
     }
 
@@ -173,6 +177,8 @@ public class PlayerController : MonoBehaviour
         sargantanaAgafada = false;
 
         particulesSargantana.Play();
+
+        meshMaTancada.enabled = false;
     }
 
     private void HandleMouseLook()
